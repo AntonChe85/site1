5a5c285882e9b3617c03753898cac9d873bb14ca
Anton Cherkasov <Anton.che85@gmail.com>
2016-07-28 21:08:55 +0300

    header

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,45 @@
 var gulp = require("gulp"),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    jade = require('gulp-jade'),
+    sass = require('gulp-sass');
 
-    gulp.task('server', function () {
-        browserSync ({
-            port: 9000,
-           server: {
-                baseDir: 'app'
-            }
-        });
-    });
+// Компиляция Jade в html
+gulp.task('jade', function () {
+  gulp.src('app/*.jade')
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('app'));
+});
 
-    gulp.task('watch', function () {
-        gulp.watch ([
-            'app/*.html',
-            'app/js/**/*.js',
-            'app/css/**/*.css'
-        ]).on('change', browserSync.reload);
-    });
+ //Компиляция SASS в CSS
+ gulp.task('sass', function () {
+   gulp.src('app/scss/*.scss')
+     .pipe(sass({pretty: true}))
+     .pipe(sass())
+     .pipe(gulp.dest('app/css'));
+ });
 
-    gulp.task('default', ['server', 'watch']);
+// Запускаем локальный сервер (только после компиляции jade&sass)
+gulp.task('server', ['jade', 'sass'], function () {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+// слежка и запуск задач
+gulp.task('watch', function () {
+  gulp.watch('app/**/*.jade', ['jade']);
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch([
+    'app/*.html',
+    'app/js/**/*.js',
+    'app/css/**/*.css'
+  ]).on('change', browserSync.reload);
+});
+
+// Задача по-умолчанию
+gulp.task('default', ['server', 'watch']);
